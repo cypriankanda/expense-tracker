3d856aa2d28ae3503676cf34de72128f428c295d
cypriankanda <cyprianboit@gmail.com>
2025-04-14 10:49:10 +0300

    vercel.json added

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/expense-tracker/', // GH Repo name
+  // No base path needed for Vercel
 })
